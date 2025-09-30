b0b8ffdc465e87d8f4845ce3e4846b63afb68462
Ayushmaan <ayushmaankumar5@gmail.com>
2025-09-30 18:09:57 +0530

    deploy

--- src/helper/axiosInstance.js
+++ src/helper/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const url = import.meta.env.VITE_URL;
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: url,
   headers: {
     "Content-Type": "application/json",
   },
